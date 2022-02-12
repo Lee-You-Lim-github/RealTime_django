05b8e001338b598e037267b9b75fbd53aa38e5a4
Lee-You-Lim-github <akfl513@naver.com>
2022-02-13 02:07:21 +0900

    Shop 모델 lat, long 필드 변경

--- shop/serializers.py
+++ shop/serializers.py
@@ -7,10 +7,9 @@ class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
         fields = [
-            "id", "shop_num", "name", "category", "address", "telephone", "opening_hours", "now_table_count",
-            "total_table_count", "notice", "intro", "photo", "registered_date", "user_id"
+            "id", "shop_num", "name", "category", "address", "lat", "long", "telephone", "opening_hours", "now_table_count",
+            "total_table_count", "conv_parking", "conv_pet", "conv_wifi", "conv_pack", "notice", "intro", "photo", "registered_date", "user_id"
         ]
-
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
