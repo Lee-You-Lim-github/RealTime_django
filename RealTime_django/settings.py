05a8d2694b7b80955784d23b99fa6c86764cd273
Lee-You-Lim-github <akfl513@naver.com>
2022-03-03 16:25:57 +0900

    static 설정 파일 추가

--- RealTime_django/settings.py
+++ RealTime_django/settings.py
@@ -147,7 +147,8 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
