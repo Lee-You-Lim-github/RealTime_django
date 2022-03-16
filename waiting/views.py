c33f4e2c7799f105bc559ffba926930941708d23
Lee-You-Lim-github <akfl513@naver.com>
2022-03-17 04:50:07 +0900

    대기 페이지네이션 구현

--- waiting/views.py
+++ waiting/views.py
@@ -1,13 +1,16 @@
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-
 from waiting.models import Waiting
 from waiting.serializers import WaitingCreateSerializer, WaitingListSerializer
-
+from django.db.models import Q
+from rest_framework import filters
+from waiting.paginations.WaitingPagination import WaitingPagination
 
 class WaitingViewSet(ModelViewSet):
     queryset = Waiting.objects.all()
+    filter_backends = [filters.OrderingFilter]
     ordering_fields = ['wait_date']
+    pagination_class = WaitingPagination
 
     def get_serializer_class(self):
         method = self.request.method
@@ -15,6 +18,21 @@ class WaitingViewSet(ModelViewSet):
             return WaitingCreateSerializer
         else:
             return WaitingListSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        query = self.request.query_params.get("query", "")
+        conditions = Q(user_id__username__icontains=query) | Q(user_id__telephone__icontains=query)
+        if query:
+            qs = qs.filter(conditions)
+
+        shop_id = self.request.query_params.get("shop_id", "")
+        shop_id_conditions = Q(shop_id__id__exact=shop_id)
+        if shop_id:
+            qs = qs.filter(shop_id_conditions)
+
+        return qs
 
     # def get_permissions(self):
     #     if self.request.method == "GET":
