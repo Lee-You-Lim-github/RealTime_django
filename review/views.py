7312693d684935608912ee5a0d9433241b711764
back hae ran <idg9833@gmail.com>
2022-03-18 00:25:33 +0900

    review view에 user_id 필터를 위한 queryset 추가

--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-
+from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -28,6 +28,16 @@ class ReviewViewSet(viewsets.ModelViewSet):
             return ReviewCreateSerializer
         else:
             return ReviewSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        user_id = self.request.query_params.get("user_id", "")
+        conditions = Q(book_id__user_id__id=user_id) | Q(wait_id__user_id__id=user_id)
+        if user_id:
+            qs = qs.filter(conditions)
+
+        return qs
 
     # def get_permissions(self):
     #     if self.request.method == "GET":
