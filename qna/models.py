4ff57eb4a290a1dfa96991e4e00533ba11059473
back hae ran <idg9833@gmail.com>
2022-03-18 23:14:58 +0900

    qna model에 answer, photo에 null=True, blank=True 추가

--- qna/models.py
+++ qna/models.py
@@ -5,8 +5,8 @@ from accounts.models import User
 class Qna(models.Model):
     qna_title = models.CharField(max_length=100)
     qna_content = models.TextField()
-    qna_answer = models.TextField()
-    qna_photo = models.TextField()
+    qna_answer = models.TextField(null=True, blank=True)
+    qna_photo = models.TextField(null=True, blank=True)
     qna_registered_date = models.DateField()
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
