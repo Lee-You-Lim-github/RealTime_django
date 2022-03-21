83b9b71868de1dae3d4937b70cd760836e8aee9e
back hae ran <idg9833@gmail.com>
2022-03-21 11:34:28 +0900

    qna models photo field 변경

--- qna/models.py
+++ qna/models.py
@@ -6,7 +6,7 @@ class Qna(models.Model):
     qna_title = models.CharField(max_length=100)
     qna_content = models.TextField()
     qna_answer = models.TextField(null=True, blank=True)
-    qna_photo = models.TextField(null=True, blank=True)
+    qna_photo = models.ImageField(null=True, blank=True)
     qna_registered_date = models.DateField(auto_now_add=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
