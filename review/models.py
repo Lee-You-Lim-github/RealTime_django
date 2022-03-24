d79f9f371d1128d5b9cd7201412dc03bc8b084e9
back hae ran <idg9833@gmail.com>
2022-03-24 18:42:06 +0900

    review  model 내부 Choices TextChoices 사용하도록 변경

--- review/models.py
+++ review/models.py
@@ -5,22 +5,21 @@ from waiting.models import Waiting
 
 
 class Review(models.Model):
+    class Choices(models.TextChoices):
+        VERY_GOOD = ("매우좋아요", "매우좋아요")
+        GOOD = ("좋아요", "좋아요")
+        NORMAL = ("보통이에요", "보통이에요")
+        BAD = ("별로예요", "별로예요")
+        VERY_BAD = ("싫어요", "싫어요")
 
-    CHOICES = (
-        ("매우좋아요", "매우좋아요"),
-        ("좋아요", "좋아요"),
-        ("보통이에요", "보통이에요"),
-        ("별로예요", "별로예요"),
-        ("싫어요", "싫어요"),
-    )
     rating = models.PositiveSmallIntegerField(
         validators=[MaxValueValidator(5)],
         default=5)
     content = models.CharField(max_length=150)
-    flavor = models.CharField(max_length=20, choices=CHOICES)
-    cleaned = models.CharField(max_length=20, choices=CHOICES)
-    kindness = models.CharField(max_length=20, choices=CHOICES)
-    mood = models.CharField(max_length=20, choices=CHOICES)
+    flavor = models.CharField(max_length=20,  choices=Choices.choices)
+    cleaned = models.CharField(max_length=20, choices=Choices.choices)
+    kindness = models.CharField(max_length=20, choices=Choices.choices)
+    mood = models.CharField(max_length=20, choices=Choices.choices)
     created_at = models.DateTimeField(auto_now_add=True)
     book_id = models.ForeignKey(Booking, null=True, blank=True, on_delete=models.CASCADE)
     wait_id = models.ForeignKey(Waiting, null=True, blank=True, on_delete=models.CASCADE)
