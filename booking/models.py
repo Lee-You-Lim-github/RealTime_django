8f7a8582a1202609f3ab025bd83d5e174c27d3d3
back hae ran <idg9833@gmail.com>
2022-03-28 11:14:17 +0900

    booking models에 지난 날짜, 시간 예약할 수 없도록 구현

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,9 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from shop.models import Shop
 from accounts.models import User
+from datetime import date
+from datetime import datetime
 
 
 class Booking(models.Model):
@@ -11,6 +14,14 @@ class Booking(models.Model):
     method = models.CharField(max_length=1)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     shop_id = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if self.day < date.today():
+            raise ValidationError("지난 날짜는 예약할 수 없습니다.")
+
+        if self.time < datetime.today().time():
+            raise ValidationError("지난 시간은 예약할 수 없습니다.")
+        super(Booking, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['day']
