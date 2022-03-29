24be77d7e89922697299125b24c344f5eb0dfff1
back hae ran <idg9833@gmail.com>
2022-03-29 17:23:39 +0900

    user Black models Fk, start_date, end_date, black_count 추가

--- user/models.py
+++ user/models.py
@@ -1,12 +1,28 @@
+from datetime import timedelta
 from django.db import models
+from django.utils import timezone
 from accounts.models import User
 from booking.models import Booking
 from shop.models import Shop
 
 
+now = timezone.now()
+
+
+def default_date():
+    return now + timedelta(days=1)
+
+
+def default_end_date():
+    return default_date() + timedelta(days=3)
+
+
 class Black(models.Model):
-    black_date = models.CharField(max_length=1)
+    start_date = models.DateField(default=default_date)
+    end_date = models.DateField(default=default_end_date)
+    black_count = models.CharField(max_length=1, default="0")
     book_id = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Pick(models.Model):
