0af1f272a7f0196ebbca562a565ef5f19a1ac015
Eunji Nam <godangel31@gmail.com>
2022-04-07 17:14:40 +0900

    waiting serializers.py 에 review_set 추가

--- waiting/serializers.py
+++ waiting/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from review.serializers import ReviewSerializer
 from waiting.models import Waiting
 
 
@@ -9,6 +11,8 @@ class WaitingCreateSerializer(serializers.ModelSerializer):
 
 
 class WaitingListSerializer(serializers.ModelSerializer):
+    review_set = ReviewSerializer(many=True, read_only=True)
+
     class Meta:
         model = Waiting
         fields = "__all__"
